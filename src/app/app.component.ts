3bad7db3a7cc3b2435005ccde5665d5348e30366
ClaireVanlerberghe <vanlerberghe24@gmail.com>
2024-09-19 10:05:08 +0200

    "test api"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'testApi';
+export class AppComponent implements OnInit {
+  imageUrl: string = ''; 
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.getImageLink();
+  }
+
+  getImageLink() {
+    this.apiService.getData().subscribe(
+      (data) => {
+        console.log('Données reçues :', data);
+        this.imageUrl = data.message; 
+      },
+      (error) => {
+        console.error('Erreur lors de la récupération du lien :', error);
+      }
+    );
+  }
+
+  reload() {
+    window.location.reload()
+  }
 }
